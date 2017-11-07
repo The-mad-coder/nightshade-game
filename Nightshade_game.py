7a4fee639c390a1bf8e2b35a20ace02b698bf901
The-mad-coder <33107023+The-mad-coder@users.noreply.github.com>
2017-11-07 16:55:26 -0600

    Add files via upload

---
+++ Nightshade_game.py
@@ -0,0 +1,18 @@
+import pygame
+pygame.init()
+a=1
+black=(0,0,0)
+white=(255,255,255)
+screen=pygame.display.set_mode((700,500))
+while a==1:
+	b=pygame.mouse.get_pos()
+	c=b[0]
+	d=b[1]
+	for event in pygame.event.get():
+		if event.type==pygame.QUIT:
+			a=2
+#font=pygame.font.SysFont('Calibri', 25,1,2)
+#text=font.render(('text'),1,black)
+#screen.blit(text,[150,100])
+	screen.fill(white)
+	pygame.display.flip()
