e30e73650e7fe9d0b83f2fe05d6eb0d5a7dddd01
The-mad-coder <33107023+The-mad-coder@users.noreply.github.com>
2017-11-07 21:30:49 -0600

    Update Nightshade_game.py

--- Nightshade_game.py
+++ Nightshade_game.py
@@ -1,9 +1,22 @@
 import pygame
+import os
 pygame.init()
 a=1
 black=(0,0,0)
 white=(255,255,255)
 screen=pygame.display.set_mode((700,500))
+gameDisplay = pygame.display.set_mode((700,500))
+hero=pygame.image.load('hero1.png')
+_image_library = {}
+def get_image(path):
+        global _image_library
+        image = _image_library.get(path)
+        if image == None:
+                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+                image = pygame.image.load(canonicalized_path)
+                _image_library[path] = image
+        return image
+print get_image('hero.png')
 #main event loop:
 while a==1:
 	b=pygame.mouse.get_pos()
@@ -15,9 +28,10 @@ while a==1:
 #font=pygame.font.SysFont('Calibri', 25,1,2)
 #text=font.render(('text'),1,black)
 #screen.blit(text,[150,100])
-	screen.fill(white)
 	#pygame.draw.rect(screen_name,color,[x coord. of top left corner,y coord. of top left corner,leght of rect.,height of rect]):draws a rectagle on a screen
 	#screen.fill(color):fills the screen with one color
 	#pygame.display.flip():shows all made changes
+	screen.fill(white)
+	screen.blit(hero, (20,20))
 	pygame.draw.rect(screen,black,[0,450,700,50],0)
 	pygame.display.flip()
